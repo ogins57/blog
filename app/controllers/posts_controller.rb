5e994ef1b9e5565046ef5b0ae700dc6e3b84b72c
荻野 浩史 <cheese.cake0611@gmail.com>
2013-04-01 02:02:46 +0900

    #38

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,44 @@
+#coding: utf-8
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+      @posts = Post.all(:order =>'created_at DESC')
   end
 
+  def show
+      @post = Post.find(params[:id])
+  end
+    
+  def new
+    @post = Post.new
+  end
+    
+  def create
+      @post = Post.new(params[:post])
+      if @post.save
+          redirect_to posts_path, notice: '正常に作成されました。'
+      else
+        render action: 'new'
+      end
+  end
+    
+  def edit
+      @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      if @post.update_attributes(params[:post])
+          redirect_to posts_path, notice: '正常に更新されました。'
+      else
+          render action: 'edit'
+      end
+  end
+    
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      render json: { post: @post }
+  end
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    
+    has_many :comments
+    
+    validates :title,   :presence => true
+    validates :content, :presence => true,
+    :length =>{:minimum =>5}
 end
